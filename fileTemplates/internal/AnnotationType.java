514290a73cbcf52fd30b11aa9a521878d5a5dff1
tobato <tobato@msn.com>
2017-09-21 15:22:36 +0800

    IU-2017.2.4 <wuyf@wuyfdeMacBook-Pro.local Create baseRefactoring.xml, markdown.xml, ide.general.xml, trainingPlugin.xml, project.default.xml, Enum.java, Interface.java, git.xml, other.xml, Default.xml, find.xml, ui.lnf.xml, filetypes.xml, projectView.xml, gradle.run.settings.xml, path.macros.xml, databaseDrivers.xml, github_settings.xml, AnnotationType.java, Class.java, jdk.table.xml, debugger.renderers.xml, laf.xml, Mac OS X 10_5_ copy.xml, editor.codeinsight.xml, databaseSettings.xml, toStringTemplates.xml, keymap.xml, editor.xml, debugger.xml

---
+++ fileTemplates/internal/AnnotationType.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * ${DESCRIPTION}
+ * @author ${USER}
+ * @create ${YEAR}-${MONTH}-${DAY} ${TIME}
+ */
+public @interface ${NAME} {
+}
---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/**
+ * ${DESCRIPTION}
+ * @author ${USER}
+ * @create ${YEAR}-${MONTH}-${DAY} ${TIME}
+ */
+public interface ${NAME} {
+}
